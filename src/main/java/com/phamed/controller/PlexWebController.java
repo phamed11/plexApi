b9ae3133b2f211cf01e3120688813bcc993112c9
Peter <wrkpeter@gmail.com>
2019-02-16 13:31:45 +0100

    basic inMemory security created

--- src/main/java/com/phamed/controller/PlexWebController.java
+++ src/main/java/com/phamed/controller/PlexWebController.java
@@ -18,6 +18,6 @@ public class PlexWebController {
   @GetMapping("/")
   public String allVideos(Model model) {
     model.addAttribute("videos", plexService.allVideos());
-    return "index";
+    return "home";
   }
 }
--- src/main/java/com/phamed/security/WebSecurityConfiguration.java
+++ src/main/java/com/phamed/security/WebSecurityConfiguration.java
@@ -1,19 +1,29 @@
 package com.phamed.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 
   @Override
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
     auth.inMemoryAuthentication()
+        .passwordEncoder(getPasswordEncoder())
         .withUser("user")
-        .password("asdfasdf")
+        .password(getPasswordEncoder().encode("asdfasdf"))
         .roles("USER");
   }
 
@@ -22,10 +32,21 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     http.csrf().disable()
         .authorizeRequests()
         .antMatchers("/").permitAll()
+        .antMatchers("/resources/**").permitAll()
         .anyRequest().hasRole("USER")
+        .and()
+        .formLogin()
+        .loginPage("/login")
+        .permitAll()
         .and()
         .logout()
         .logoutUrl("/logout")
         .permitAll();
+  }
+
+
+  @Bean
+  PasswordEncoder getPasswordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 }
