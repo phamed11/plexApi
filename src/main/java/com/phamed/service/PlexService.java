42e17dd308d6d0fd078984b31eda242860379aed
Peter <wrkpeter@gmail.com>
2019-02-16 12:53:17 +0100

    basic inMemory security created

--- src/main/java/com/phamed/service/PlexService.java
+++ src/main/java/com/phamed/service/PlexService.java
@@ -2,7 +2,6 @@ package com.phamed.service;
 
 import com.phamed.plexModels.MediaContainer;
 import com.phamed.plexModels.Video;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.xml.bind.JAXBContext;
